c29f0546c753380edb6bb311053e372f1359462c
LyubovTa-na <ltalapina464@gmail.com>
2021-01-28 19:20:20 +0500

    Метод итераций

--- 7/7.cpp
+++ 7/7.cpp
@@ -8,9 +8,53 @@ void newton(); //Метод Ньютона
 void divide(); //Метод половинного деления
 void iterations(); //Метод итераций
 
-
 using namespace std;
+
+int left = 2, right = 3; //левая и правая границы
+double eps = 0.0001; //Точность eps
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    setlocale(LC_ALL, "rus");
+
+    cout << "Точное значение корня: 2.2985" << endl;
+
+    newton();
+    divide();
+    iterations();
+}
+
+double F(double x)
+{
+    return (3 * sin(sqrt(x)) + 0.35 * x - 3.8);
+}
+
+double dF(double x)
+{
+    return (3 * cos(sqrt(x)) * (1 / (2 * sqrt(x))) + 0.35);
+}
+
+void newton()
+{
+    //
+}
+
+void divide()
+{
+    double l = ::left, r = ::right;
+    double x;
+    do
+    {
+        x = (r + l) / 2.0; //середина отрезка
+
+        if (F(l) * F(x) < 0) r = x; //Проверка на разность знаков
+        else l = x;
+    } while (abs(l - r) >= eps);
+
+    cout << "Решение методом половинного деления: " << x << endl;
+}
+
+void iterations()
+{
+    //
 }
